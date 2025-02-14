d593f64f403bfff7005198347ba2d8cc2990aa3b
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2025-02-14 12:25:52 -0600

    Relaciones en colecciones

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection({
     type: 'content',
@@ -9,12 +9,21 @@ const blogCollection = defineCollection({
         image: z.string(),
 
         // Relación
-        author: z.string(),
+        //author: z.string(),
+        author: reference('author'),
 
         // Relación
         tags: z.array(z.string()),
     }),
 });
+
+const authorCollection = defineCollection({
+    type: 'data',
+    schema:({image}) => z.object({
+        name: z.string(),
+        avatar: image(),
+    })
+})
 
 export const collections = {
     blog: blogCollection,
